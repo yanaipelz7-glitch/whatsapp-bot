df2d7cbfa95d8534f52256ae0ec5dbc741ea6cbf
yanaipelz7-glitch <yanaipelz7@gmail.com>
2026-08-24 23:39:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,17 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ],
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
     }
 });
 
